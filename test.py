405e2b0f10133761b56da3acd425591cdb8311ee
Alen Antony <alenantonyme@gmail.com>
2023-04-12 06:48:34 +0530

    Some file structuring

--- test.py
+++
@@ -1,6 +0,0 @@
-import client as c
-
-bob=c.User('Bob')
-bob.publish()
-print(c.server.get('Bob'))
-print(c.server.get('Bo'))
--- test_server.py
+++
@@ -1,6 +0,0 @@
-import server as s
-
-k=s.Server()
-print(k.get_key_bundle('alen'))
-
-
